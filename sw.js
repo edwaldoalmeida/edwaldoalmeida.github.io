4c0504502c8401417331f0c2577e5df2e31035a4
Edwaldo Almeida <contact@edwaldoalmeida.com>
2019-06-11 07:44:37 -0400

    updating

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2d2dcd4368ec4ef513de.js"
+    "url": "webpack-runtime-a00b348f3d29704e6100.js"
   },
   {
     "url": "app-021c412e11a397732d63.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "00eaa5c9015d426672b27a11c91ece0c"
+    "revision": "2f78bc1de0bc7452f24a09dbf6a3afeb"
   },
   {
     "url": "styles.19364f3221d0b3f8ad64.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
     "url": "1-acf3e5857ed639bfaeda.js"
   },
   {
-    "url": "component---src-pages-404-js-af08842062d123627d36.js"
+    "url": "component---src-pages-404-js-8ccb449e62b24af8ae12.js"
   },
   {
     "url": "styles-59027054298d30e98e79.js"
